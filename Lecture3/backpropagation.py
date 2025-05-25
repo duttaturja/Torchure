06ce8201d6f55f062ddff1255978fcc7fca94d4a
duttaturja <duttaturja@gmail.com>
2025-05-26 01:05:25 +0600

    L4: Backpropagation

---
+++ Lecture3/backpropagation.py
@@ -0,0 +1,26 @@
+import torch
+
+x = torch.tensor(1.0)
+y = torch.tensor(2.0)
+
+w = torch.tensor(1.0, requires_grad=True)
+
+# Forward pass and compute loss
+y_hat = w * x
+loss = (y_hat - y) ** 2
+
+print(f"Loss: {loss.item()}")
+
+
+# Backward pass
+loss.backward()
+print(f"Gradient: {w.grad.item()}")
+
+# Update weights
+## next forward and backward pass
+learning_rate = 0.1
+
+with torch.no_grad():
+    w -= learning_rate * w.grad
+    print(f"Updated weight: {w.item()}")
+    w.grad.zero_()  # Reset gradients for the next iteration
