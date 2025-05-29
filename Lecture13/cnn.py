165ef3c87a73b6586c764240d056cef84c64afba
duttaturja <duttaturja@gmail.com>
2025-05-29 22:58:40 +0600

    L13: CNN

--- Lecture13/cnn.py
+++ Lecture13/cnn.py
@@ -20,21 +20,16 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-# CIFAR10: 60000 32x32 color images in 10 classes, with 6000 images per class
-train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
+# CIFAR10: dataset 
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 
-test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
+test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
-                                          shuffle=True)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
-                                         shuffle=False)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 def imshow(img):
     img = img / 2 + 0.5  # unnormalize
@@ -97,8 +92,8 @@ for epoch in range(num_epochs):
             print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
 
 print('Finished Training')
-PATH = './Models/cnn.pth'
-torch.save(model.state_dict(), PATH)
+path = './Models/cnn.pth'
+torch.save(model.state_dict(), path)
 
 with torch.no_grad():
     n_correct = 0
